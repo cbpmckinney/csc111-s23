50c2239225ce67c2b6d86ad13d7885c0b94b947f
Colin McKinney <mckinnec@wabash.edu>
2023-01-19 11:50:54 -0500

    Initial commit

--- math/mathdemo2.py
+++
@@ -1,9 +0,0 @@
-
-principal = 55000
-interestrate = .05
-num = 12 #12 months per year, compounding monthly
-timet = 5 #5 years
-
-amount = principal*(1+interestrate/num)**(num*timet)
-
-print("The accrued amount is:", round(amount,2))
