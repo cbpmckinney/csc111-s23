9a5920c43df206882f03ca1e6315b6ba0529845f
Colin McKinney <mckinnec@wabash.edu>
2023-01-25 07:51:58 -0500

    Added loop stuff

---
+++ input/input1.py
@@ -0,0 +1,10 @@
+# How to accept (and use) user input
+
+myvar = input("Please type a number: ")
+mynum = float(myvar)
+
+print(myvar)
+print(type(myvar))
+
+print(mynum)
+print(type(mynum))
---
+++ loops/loops1.py
@@ -0,0 +1,6 @@
+
+num = int(input("Please type a number: "))
+
+for i in range(1,num+1):
+    print(i)
+
