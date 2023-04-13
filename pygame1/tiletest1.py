31123b4fefbcedd93b2b18f3244bfdd15f72bd7a
Colin McKinney <42683765+cbpmckinney@users.noreply.github.com>
2023-04-12 21:04:07 -0400

    Added pygame stuff

---
+++ pygame1/tiletest1.py
@@ -0,0 +1,112 @@
+import pygame
+from pygame.locals import *
+import numpy as np 
+
+class Tileset:
+    def __init__(self, file, size=(16, 16), margin=1, spacing=1):
+        self.file = file
+        self.size = size
+        self.margin = margin
+        self.spacing = spacing
+        self.image = pygame.image.load(file)
+        self.rect = self.image.get_rect()
+        self.tiles = []
+        self.load()
+
+    def load(self):
+
+        self.tiles = []
+        x0 = y0 = self.margin
+        w, h = self.rect.size
+        dx = self.size[0] + self.spacing
+        dy = self.size[1] + self.spacing
+
+        for x in range(x0, w, dx):
+            for y in range(y0, h, dy):
+                tile = pygame.Surface(self.size)
+                tile.blit(self.image, (0, 0), (x, y, *self.size))
+                self.tiles.append(tile)
+
+    def __str__(self):
+        return f'{self.__class__.__name__} file:{self.file} tile:{self.size}'
+
+class Game:
+    W = 640
+    H = 480
+    SIZE = W, H
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(Game.SIZE)
+        pygame.display.set_caption("Pygame Tiled Demo")
+  
+        
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+
+                elif event.type == KEYDOWN:
+                    if event.key == K_r:
+                        mytilemap.set_zero()
+                        mytilemap.render()
+            #mytilemap.render()
+        pygame.quit()
+
+    def load_image(self, file):
+        self.file = file
+        self.image = pygame.image.load(file)
+        self.rect = self.image.get_rect()
+
+        self.screen = pygame.display.set_mode(self.rect.size)
+        pygame.display.set_caption(f'size:{self.rect.size}')
+        self.screen.blit(self.image, self.rect)
+        pygame.display.update()
+
+
+class Tilemap:
+    def __init__(self, tileset, size=(10, 20), rect=None):
+        self.size = size
+        self.tileset = tileset
+        self.map = np.zeros(size, dtype=int)
+
+        h, w = self.size
+        self.image = pygame.Surface((16*w, 16*h))
+        if rect:
+            self.rect = pygame.Rect(rect)
+        else:
+            self.rect = self.image.get_rect()
+
+    def render(self):
+        m, n = self.map.shape
+        for i in range(m):
+            for j in range(n):
+                tile = self.tileset.tiles[self.map[i, j]]
+                self.image.blit(tile, (j*16, i*16))
+
+    def set_zero(self):
+        self.map = np.zeros(self.size, dtype=int)
+        print(self.map)
+        print(self.map.shape)
+        self.render()
+
+    def set_random(self):
+        n = len(self.tileset.tiles)
+        self.map = np.random.randint(n, size=self.size)
+        print(self.map)
+        self.render()
+
+    def __str__(self):
+        return f'{self.__class__.__name__} {self.size}'
+
+
+myfile = 'Retro-Lines-Tiles-transparent.png'
+
+mytileset = Tileset(myfile)
+mytilemap = Tilemap(mytileset)
+
+game = Game()
+game.run()
