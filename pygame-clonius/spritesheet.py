60f787ecbc78431d3c9fc435a0646d0aad2825b4
Colin McKinney <42683765+cbpmckinney@users.noreply.github.com>
2023-05-03 21:00:29 -0400

    Updated spritesheets to use chroma keys

--- pygame-clonius/spritesheet.py
+++ pygame-clonius/spritesheet.py
@@ -1,14 +1,15 @@
 import pygame
 
+
 class spritesheet(object):
 
     def __init__(self, filename):
         self.sheet = pygame.image.load(filename).convert()
+        self.transColor = self.sheet.get_at((0, 0))
 
     def image_at(self, rectangle, colorkey=None):
         rect = pygame.Rect(rectangle)
         image = pygame.Surface(rect.size).convert()
-        image.blit(self.sheet, (0,0), rect)
-        image.set_alpha(255)
+        image.blit(self.sheet, (0, 0), rect)
+        image.set_colorkey(self.transColor)
         return image
- 
